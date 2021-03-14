e31984b3579e0dfc161250679cb75d71a58b286d
wanarisu <wanarisu@gmail.com>
2021-03-14 14:05:17 +0900

    マスター登録

---
+++ File_tools/Assets/Scripts/API_test.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Net;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+public class API_test : MonoBehaviour
+{
+    //接続するURL
+    private const string URL = @"https://192.168.0.219/test_img.png";
+    //private const string URL = @"https://192.168.0.219/Tools/index.php/api/test/test";
+    //private const string URL = @"http://cdn-www.dailypuppy.com/media/dogs/anonymous/coffee_poodle01.jpg_w450.jpg";
+    public Text text;
+
+    //ゲームオブジェクトUI > ButtonのInspector > On Click()から呼び出すメソッド
+    public void OnClick()
+    {
+        //コルーチンを呼び出す
+        StartCoroutine("GET", URL+ "?0=100");
+        StartCoroutine("POST", URL);
+    }
+
+    //コルーチン
+    IEnumerator GET(string url)
+    {
+        //URLをGETで用意
+        UnityWebRequest webRequest = UnityWebRequest.Get(url);
+        webRequest.certificateHandler = new AcceptAllCertificatesSignedWithASpecificKeyPublicKey();//追加
+        //URLに接続して結果が戻ってくるまで待機
+        yield return webRequest.SendWebRequest();
+
+        //エラーが出ていないかチェック
+        if (webRequest.isNetworkError)
+        {
+            text.text = webRequest.error;
+            //通信失敗
+            Debug.Log(webRequest.error);
+        }
+        else
+        {
+            text.text = webRequest.downloadHandler.text;
+
+            //通信成功
+            Debug.Log(webRequest.downloadHandler.text);
+        }
+    }
+
+    //コルーチン
+    IEnumerator POST(string url)
+    {
+        //POSTする情報
+        WWWForm form = new WWWForm();
+        form.AddField("zipcode", 1000001);
+
+        //URLをPOSTで用意
+        UnityWebRequest webRequest = UnityWebRequest.Post(url, form);
+        //UnityWebRequestにバッファをセット
+        webRequest.downloadHandler = new DownloadHandlerBuffer();
+        //URLに接続して結果が戻ってくるまで待機
+        yield return webRequest.SendWebRequest();
+
+        //エラーが出ていないかチェック
+        if (webRequest.isNetworkError)
+        {
+            text.text += webRequest.error;
+            //通信失敗
+            Debug.Log(webRequest.error);
+        }
+        else
+        {
+            text.text += webRequest.downloadHandler.text;
+
+            //通信成功
+            Debug.Log(webRequest.downloadHandler.text);
+        }
+    }
+}
+
+
+
+class AcceptAllCertificatesSignedWithASpecificKeyPublicKey : CertificateHandler
+{
+    // Encoded RSAPublicKey
+    private static string PUB_KEY = "30818902818100C4A06B7B52F8D17DC1CCB47362" +
+        "C64AB799AAE19E245A7559E9CEEC7D8AA4DF07CB0B21FDFD763C63A313A668FE9D764E" +
+        "D913C51A676788DB62AF624F422C2F112C1316922AA5D37823CD9F43D1FC54513D14B2" +
+        "9E36991F08A042C42EAAEEE5FE8E2CB10167174A359CEBF6FACC2C9CA933AD403137EE" +
+        "2C3F4CBED9460129C72B0203010001";
+    protected override bool ValidateCertificate(byte[] certificateData)
+    {
+        X509Certificate2 certificate = new X509Certificate2(certificateData);
+        string pk = certificate.GetPublicKeyString();
+        if (pk.Equals(PUB_KEY))//公開鍵があっているか検証
+            return true;
+        // Bad dog
+        return false;
+    }
+}
---
+++ File_tools/Assets/Scripts/FileManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+public class FileManager : MonoBehaviour
+{
+    private int nowPage = 0;	// 今のページ
+	private string[] filename;  // ファイルたち
+
+	public string path = @"H:\共有\エロ漫画\##未使用##\(同人CG集) [Master-random] トロピカルワイフ～出張先の南国で人妻エッチ!～\";    // パス
+
+	public List<GameObject>	imageList;		// 画像リスト
+	public GameObject		imagePlefab;    // イメージ作成用のプレハブ
+
+	public GameObject mainObj;
+
+	public Vector2 pos;						// 画像の位置
+    void Start()
+    {
+		//try
+		//{
+			filename = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+			//foreach (string f in filename)
+			//{
+			//	Debug.Log(f);
+			//}
+		//}
+		//catch (Exception e)
+		//{
+		//	Console.WriteLine(e.Message);
+		//}
+
+		// 自分のデータ取得
+		imageList = new List<GameObject>();
+
+		pos = mainObj.transform.position;
+	}
+
+	void Update()
+    {
+		if (nowPage < filename.Length)
+		{
+			// オブジェクト作成
+			GameObject obj = GameObject.Instantiate(imagePlefab, pos, Quaternion.identity);
+
+			Debug.Log(pos);
+			Debug.Log(obj.transform.position);
+			Debug.Log(mainObj.transform.position);
+
+			// 画像作成
+			Image image = obj.GetComponent<Image>();
+			Texture2D texture2D = GetTexture(filename[nowPage]);
+			image.sprite = Sprite.Create(texture2D, new Rect(0, 0, texture2D.width, texture2D.height), Vector2.zero);
+
+			// 画像オブジェのサイズ設定
+			RectTransform rectTransform = obj.GetComponent<RectTransform>();
+			rectTransform.sizeDelta = new Vector2(texture2D.width, texture2D.height);
+
+			// 自分の子供に設定
+			obj.transform.parent = mainObj.transform;
+
+			// リストに追加
+			imageList.Add(obj);
+
+			// 次の準備
+			pos.y -= texture2D.height - 10;
+
+			nowPage++;
+		}
+	}
+
+	public byte[] readPngFile(string path)
+	{
+		using (FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read))
+		{
+			BinaryReader bin = new BinaryReader(fileStream);
+			byte[] values = bin.ReadBytes((int)bin.BaseStream.Length);
+			bin.Close();
+			return values;
+		}
+	}
+
+	public Texture2D readByBinary(byte[] bytes)
+	{
+		Texture2D texture = new Texture2D(1, 1);
+		texture.LoadImage(bytes);
+		return texture;
+	}
+
+	public Texture2D GetTexture(string path)
+	{
+		return readByBinary(readPngFile(path));
+	}
+}
