750133ae746ee0a3ed705062b2fc0cabfbcf8b62
wanarisu <wanarisu@gmail.com>
2021-03-14 18:49:28 +0900

    POST成功

--- File_tools/Assets/Scripts/API_test.cs
+++ File_tools/Assets/Scripts/API_test.cs
@@ -4,62 +4,58 @@ using System.Text;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
-using System.Security.Cryptography;
-using System.Security.Cryptography.X509Certificates;
+using System.Collections.Generic;
 
 public class API_test : MonoBehaviour
 {
 	//接続するURL
 	//private const string URL = @"https://192.168.0.219/test_img.png";
-	private const string URL = @"http://192.168.0.219:8080/index.php/Tools/api/test/post_test";
+	private const string URL = @"http://192.168.0.219:8080/Tools/api/test/post_test";
 	//private const string URL = @"http://cdn-www.dailypuppy.com/media/dogs/anonymous/coffee_poodle01.jpg_w450.jpg";
 	public Text text;
 
     //ゲームオブジェクトUI > ButtonのInspector > On Click()から呼び出すメソッド
     public void OnClick()
     {
-        //コルーチンを呼び出す
-        //StartCoroutine("GET", URL+ "?0=100");
-        StartCoroutine("POST", URL);
+		//コルーチンを呼び出す
+		StartCoroutine("POST", URL);
+		StartCoroutine("GET", "http://192.168.0.219:8080/Tools/api/test/get_test?0=100");
     }
 
-    //コルーチン
-    IEnumerator GET(string url)
+    private IEnumerator GET(string url)
     {
-        //URLをGETで用意
-        UnityWebRequest webRequest = UnityWebRequest.Get(url);
-        //URLに接続して結果が戻ってくるまで待機
-        yield return webRequest.SendWebRequest();
-
-        //エラーが出ていないかチェック
-        if (webRequest.isNetworkError)
+        using (var webRequest = UnityWebRequest.Get(url))
         {
-            text.text = webRequest.error;
-            //通信失敗
-            Debug.Log(webRequest.error);
-        }
-        else
-        {
-            text.text = webRequest.downloadHandler.text;
+            webRequest.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+            yield return webRequest.SendWebRequest();
 
-            //通信成功
-            Debug.Log(webRequest.downloadHandler.text);
+            //エラーが出ていないかチェック
+            if (webRequest.isNetworkError)
+            {
+                text.text = webRequest.error;
+                //通信失敗
+                Debug.Log(webRequest.error);
+            }
+            else
+            {
+                text.text = webRequest.downloadHandler.text;
+
+                //通信成功
+                Debug.Log(webRequest.downloadHandler.text);
+            }
         }
     }
 
     //コルーチン
     IEnumerator POST(string url)
     {
-        //POSTする情報
         WWWForm form = new WWWForm();
-        form.AddField("0", 1000001);
+        form.AddField("zipcode", 1000001);
 
-        //URLをPOSTで用意
         UnityWebRequest webRequest = UnityWebRequest.Post(url, form);
-		//UnityWebRequestにバッファをセット
-		webRequest.downloadHandler = new DownloadHandlerBuffer();
 
-		webRequest.SetRequestHeader("Content-Type", "applicationx-www-form-urlencoded;charset=utf-8");
+        //UnityWebRequestにバッファをセット
+        webRequest.downloadHandler = new DownloadHandlerBuffer();
         //URLに接続して結果が戻ってくるまで待機
         yield return webRequest.SendWebRequest();
 
